122a95fce82c649fef253b33298087020418a50e
rupeshRN <51730556+rupeshRN@users.noreply.github.com>
2026-08-31 18:30:04 +0000

    Update repo1 with latest files from repo2

--- app/src/main/java/com/varsel/expensetracker/ui/transaction/components/CategoryCard.kt
+++ app/src/main/java/com/varsel/expensetracker/ui/transaction/components/CategoryCard.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.varsel.expensetracker.category.CategoryUi
 
 @Composable
@@ -85,4 +84,3 @@ fun CategoryCard(
         }
     }
 }
-
